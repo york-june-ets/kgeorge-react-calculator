adf31a77e0dc6e09ff8beab18da8bfd2c7e7e17d
Kayla George <kaylatrinitygeorge@gmail.com>
2025-06-11 12:11:48 -0400

    adding const variable formula and addToFormula method to append to formula on button clicks

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,42 +1,48 @@
-function Calculator() {
+import React, {useState} from 'react'
 
+function Calculator() {
+    const [formula, setFormula] = useState<string>('');
+
+    const addToFormula = (char: string) => {
+        setFormula(formula + char)
+    }
 
 
     return (
         <div id="calculator">
-            <div id="screen"></div>
+            <div id="screen">{formula}</div>
             <div id="buttons">
                 <div id="buttons-grid">
                     <div className="buttons-row">
                         <button id="clear">AC</button> 
-                        <button id="divide">&divide;</button> 
-                        <button id="multiply">x</button>
+                        <button id="divide" onClick={() => addToFormula("/") }>&divide;</button> 
+                        <button id="multiply" onClick={() => addToFormula("*")}>x</button>
                     </div>
                     <div className="buttons-row">
-                        <button id="7">7</button> 
-                        <button id="8">8</button> 
-                        <button id="9">9</button>
+                        <button id="7" onClick={() => addToFormula("7")}>7</button> 
+                        <button id="8" onClick={() => addToFormula("8")}>8</button> 
+                        <button id="9" onClick={() => addToFormula("9")}>9</button>
                     </div>
                     <div className="buttons-row">
-                        <button id="4">4</button> 
-                        <button id="5">5</button> 
-                        <button id="6">6</button>
+                        <button id="4" onClick={() => addToFormula("4")}>4</button> 
+                        <button id="5" onClick={() => addToFormula("5")}>5</button> 
+                        <button id="6" onClick={() => addToFormula("6")}>6</button>
                     </div>
                     <div className="buttons-row">
-                        <button id="1">1</button> 
-                        <button id="2">2</button> 
-                        <button id="3">3</button>
+                        <button id="1" onClick={() => addToFormula("1")}>1</button> 
+                        <button id="2" onClick={() => addToFormula("2")}>2</button> 
+                        <button id="3" onClick={() => addToFormula("3")}>3</button>
                     </div>
                     <div className="buttons-row">
-                        <button id=".">.</button> 
-                        <button id="0">0</button> 
-                        <button id="%">%</button>
+                        <button id="." onClick={() => addToFormula(".")}>.</button> 
+                        <button id="0" onClick={() => addToFormula("0")}>0</button> 
+                        <button id="%" onClick={() => addToFormula("%")}>%</button>
                     </div>
                 </div>
                 <div id="buttons-sidebar">
                     <button id="delete">&#x2326;</button>
-                    <button id="-">-</button>
-                    <button id="+">+</button>
+                    <button id="-" onClick={() => addToFormula("-")}>-</button>
+                    <button id="+" onClick={() => addToFormula("+")}>+</button>
                     <button id="=">=</button>
                 </div>
             </div>
