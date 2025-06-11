dcb202495cd77248617e9d5499459966174734f8
Kayla George <kaylatrinitygeorge@gmail.com>
2025-06-11 14:55:49 -0400

    creating calculator layout in new calculator css file and renaming invalid button ids

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import './Calculator.css';
 
 function Calculator() {
     const [formula, setFormula] = useState<string>('');
@@ -35,31 +36,31 @@ function Calculator() {
                         <button id="multiply" onClick={() => addToFormula("*")}>x</button>
                     </div>
                     <div className="buttons-row">
-                        <button id="7" onClick={() => addToFormula("7")}>7</button> 
-                        <button id="8" onClick={() => addToFormula("8")}>8</button> 
-                        <button id="9" onClick={() => addToFormula("9")}>9</button>
+                        <button id="seven" onClick={() => addToFormula("7")}>7</button> 
+                        <button id="eight" onClick={() => addToFormula("8")}>8</button> 
+                        <button id="nine" onClick={() => addToFormula("9")}>9</button>
                     </div>
                     <div className="buttons-row">
-                        <button id="4" onClick={() => addToFormula("4")}>4</button> 
-                        <button id="5" onClick={() => addToFormula("5")}>5</button> 
-                        <button id="6" onClick={() => addToFormula("6")}>6</button>
+                        <button id="four" onClick={() => addToFormula("4")}>4</button> 
+                        <button id="five" onClick={() => addToFormula("5")}>5</button> 
+                        <button id="six" onClick={() => addToFormula("6")}>6</button>
                     </div>
                     <div className="buttons-row">
-                        <button id="1" onClick={() => addToFormula("1")}>1</button> 
-                        <button id="2" onClick={() => addToFormula("2")}>2</button> 
-                        <button id="3" onClick={() => addToFormula("3")}>3</button>
+                        <button id="one" onClick={() => addToFormula("1")}>1</button> 
+                        <button id="two" onClick={() => addToFormula("2")}>2</button> 
+                        <button id="three" onClick={() => addToFormula("3")}>3</button>
                     </div>
                     <div className="buttons-row">
-                        <button id="." onClick={() => addToFormula(".")}>.</button> 
-                        <button id="0" onClick={() => addToFormula("0")}>0</button> 
-                        <button id="%" onClick={() => addToFormula("%")}>%</button>
+                        <button id="decimal" onClick={() => addToFormula(".")}>.</button> 
+                        <button id="zero" onClick={() => addToFormula("0")}>0</button> 
+                        <button id="mod" onClick={() => addToFormula("%")}>%</button>
                     </div>
                 </div>
                 <div id="buttons-sidebar">
                     <button id="delete" onClick={backspace}>&#x2326;</button>
-                    <button id="-" onClick={() => addToFormula("-")}>-</button>
-                    <button id="+" onClick={() => addToFormula("+")}>+</button>
-                    <button id="=" onClick={calculate}>=</button>
+                    <button id="minus" onClick={() => addToFormula("-")}>-</button>
+                    <button id="plus" onClick={() => addToFormula("+")}>+</button>
+                    <button id="equals" onClick={calculate}>=</button>
                 </div>
             </div>
         </div>
